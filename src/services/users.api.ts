a0968d84a007912593ebe476dcafec6d0a1b0f37
Nisal-Kawshalya <ict22053@std.uwu.ac.lk>
2026-01-14 17:27:35 +0530

    create Edit Profile and Logout button and theirs controls

--- src/services/users.api.ts
+++ src/services/users.api.ts
@@ -1,5 +1,6 @@
 import { db } from "../config/firebase";
 import { doc, setDoc, getDoc } from "firebase/firestore";
+import { updateDoc } from "firebase/firestore";
 
 /**
  * Create user profile in Firestore
@@ -21,9 +22,7 @@ export const createUserProfile = async (
   });
 };
 
-/**
- * Get user profile from Firestore
- */
+/* Get user profile from Firestore */
 export const getUserProfile = async (uid: string) => {
   const userRef = doc(db, "users", uid);
   const snapshot = await getDoc(userRef);
@@ -33,4 +32,15 @@ export const getUserProfile = async (uid: string) => {
   }
 
   return snapshot.data();
+};
+
+export const updateUserProfile = async (
+  uid: string,
+  name: string
+) => {
+  const userRef = doc(db, "users", uid);
+
+  await updateDoc(userRef, {
+    name,
+  });
 };
